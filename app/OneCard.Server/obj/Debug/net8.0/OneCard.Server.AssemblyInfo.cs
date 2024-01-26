15041d3fdf47a69b46ad9a085119c365ea2dbf52
Josh Flatt <josh.m.flatt@gmail.com>
2024-01-25 18:23:38 -0700

    Commented out failing deploy portion

--- app/OneCard.Server/obj/Debug/net8.0/OneCard.Server.AssemblyInfo.cs
+++ app/OneCard.Server/obj/Debug/net8.0/OneCard.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a78149d8326834d44fdf895b3911a680cc5c4450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0494751d233701f33e8a2dbbd97463e782e44cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
