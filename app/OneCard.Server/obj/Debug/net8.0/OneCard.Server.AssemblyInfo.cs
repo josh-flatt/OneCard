68b57414cbc3bf6edd15bff97b78b28760ddb525
Josh Flatt <josh.m.flatt@gmail.com>
2024-01-28 12:38:52 -0700

    added 'engine' requirement for 'node' of 20.11.0 or greater

--- app/OneCard.Server/obj/Debug/net8.0/OneCard.Server.AssemblyInfo.cs
+++ app/OneCard.Server/obj/Debug/net8.0/OneCard.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db24bca0bf05be1e7d316664716578ed6349924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34cb5f735ff8e40e16ac353037ee542f1f056c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
