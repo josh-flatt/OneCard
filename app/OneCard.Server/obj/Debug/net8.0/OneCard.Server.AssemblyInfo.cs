8abf3a31c0a08448d9213a12532ca157646e6ede
Josh Flatt <josh.m.flatt@gmail.com>
2024-01-28 15:12:03 -0700

    changed proxy conf

--- app/OneCard.Server/obj/Debug/net8.0/OneCard.Server.AssemblyInfo.cs
+++ app/OneCard.Server/obj/Debug/net8.0/OneCard.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c27cfb446cd95bc0da041252c74eb43e2867858")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c757812419ba3f836fb9772a1a6244edb3d3fcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneCard.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- app/onecard.client/src/proxy.conf.js
+++ app/onecard.client/src/proxy.conf.js
@@ -3,7 +3,8 @@ const PROXY_CONFIG = [
     context: [
       "/weatherforecast",
     ],
-    target: "https://localhost:7026",
+    // target: "https://localhost:7026",
+    target: "https://onecard.azurewebsites.net/weatherforecast",
     secure: false
   }
 ]
