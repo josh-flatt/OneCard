188eb0bcbf4e60ddd954cf23c24a14bb4bb4f6f0
Josh Flatt <josh.m.flatt@gmail.com>
2024-01-28 19:56:24 -0700

    added backend

---
+++ app/OneClient.server/OneClient.server/Controllers/SuperHeroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OneClient.server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero> {
+                new SuperHero
+                {
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+        }
+    }
+}
