5d009bdcfa5a15066a0447e8bf65aebfb75ec660
Josh Flatt <josh.flatt@unco.edu>
2024-02-05 08:12:17 -0700

    re-created backend to enable docker usage. added build script

---
+++ app/OneCard.server/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy =>
+    {
+        // policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("https://onecard-client-m854g.ondigitalocean.app").AllowAnyMethod().AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("SuperHeroOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
