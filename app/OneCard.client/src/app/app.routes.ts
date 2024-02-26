24bee51439c83d9b276a3776c85c7b3150f2be3b
Josh Flatt <josh.flatt@unco.edu>
2024-02-26 13:21:22 -0700

    changed icon and browser title; changed toolbar to card style

--- app/OneCard.client/src/app/app.routes.ts
+++ app/OneCard.client/src/app/app.routes.ts
@@ -5,10 +5,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+
+    //Home Page
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+
+    //Other Pages
     { path: 'crisis-list', component: CrisisListComponent },
     { path: 'heroes-list', component: HeroesListComponent },
-    { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
 
     //404 Page - must be last path in this array.
     { path: '**', component: PageNotFoundComponent },
--- app/OneCard.server/obj/Debug/net8.0/OneCard.server.AssemblyInfo.cs
+++ app/OneCard.server/obj/Debug/net8.0/OneCard.server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OneCard.server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba549a96b8620df417db692c2aeb0730cc3fb43c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a2f3f184039e37f662a7a03c30e766a1e0e5cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("OneCard.server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OneCard.server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
