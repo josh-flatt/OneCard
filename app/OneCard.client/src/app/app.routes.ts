4a8034c1faba15da0e3b09acc10715e112975e10
Josh Flatt <josh.m.flatt@gmail.com>
2024-02-29 17:52:41 -0700

    i did something

--- app/OneCard.client/src/app/app.routes.ts
+++ app/OneCard.client/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
 
@@ -14,6 +16,9 @@ export const routes: Routes = [
     { path: 'crisis-list', component: CrisisListComponent },
     { path: 'heroes-list', component: HeroesListComponent },
 
+    //Log In - Log Out
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
 
     //404 Page - must be last path in this array.
     { path: '**', component: PageNotFoundComponent },
