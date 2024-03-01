a971e0eee064532d6bd94aee391db248113fb4df
CounterDiamond <counteract100@gmail.com>
2024-02-29 18:25:09 -0700

    created about-us page

--- app/OneCard.client/src/app/app.routes.ts
+++ app/OneCard.client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
 
@@ -13,6 +14,7 @@ export const routes: Routes = [
     //Other Pages
     { path: 'crisis-list', component: CrisisListComponent },
     { path: 'heroes-list', component: HeroesListComponent },
+    { path: 'about-us', component: AboutUsComponent },
 
 
     //404 Page - must be last path in this array.
