a64ce26f1d819c54d7149c36b4eea72f82fa3963
Josh Flatt <josh.m.flatt@gmail.com>
2024-03-31 17:40:41 -0600

    re-enabled authentication. removed SSR

--- app/OneCard.client/src/app/app.routes.ts
+++ app/OneCard.client/src/app/app.routes.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 
 import { PackagesComponent } from './packages/packages.component';
-// import { LoginComponent } from './login/login.component';
-// import { LogoutComponent } from './logout/logout.component';
-// import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
 
@@ -16,13 +16,13 @@ export const routes: Routes = [
 
     //Other Pages
     { path: 'about-us', component: AboutUsComponent },
-    { path: 'packages', component: PackagesComponent},
+    { path: 'packages', component: PackagesComponent },
 
 
-    //Authentication - temporarily commented out
-    // { path: 'login', component: LoginComponent },
-    // { path: 'logout', component: LogoutComponent },
-    // { path: 'profile', component: UserProfileComponent },
+    // Authentication - temporarily commented out
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: 'profile', component: UserProfileComponent },
 
     //404 Page - must be last path in this array.
     { path: '**', component: PageNotFoundComponent },
