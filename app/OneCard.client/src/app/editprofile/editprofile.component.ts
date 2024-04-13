e58c474112a23bfbbf5d5d332dd6d71118fac16d
Andrewholmes22 <111909041+Andrewholmes22@users.noreply.github.com>
2024-04-13 15:45:15 -0600

    edit profile created

---
+++ app/OneCard.client/src/app/editprofile/editprofile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editprofile',
+  standalone: true,
+  imports: [],
+  templateUrl: './editprofile.component.html',
+  styleUrl: './editprofile.component.css'
+})
+export class EditprofileComponent {
+
+}
